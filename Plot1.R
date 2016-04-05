4f2baedb75bb8538ab491bb748fa42834cd5e316
uzera <aashktorab@iso.com>
2016-04-04 22:26:24 -0400

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,20 +1,9 @@
-plot1 <- function() {
-    
-    # Read Data
-    filename <- "household_power_consumption.txt"
-    DF <- read.table(filename, header=TRUE, sep=";", colClasses = "character")
-    
-    # Set required date range
-    beforeDate <- as.Date("2007/02/01", "%Y/%m/%d")
-    afterDate <- as.Date("2007/02/02", "%Y/%m/%d")
-    
-    # retrieve required subset
-    data <- subset(DF, as.Date(DF[,1], "%d/%m/%Y") >= beforeDate & as.Date(DF[,1], "%d/%m/%Y") <= afterDate)
-    
-    # Create histogram and save as png
-    png(filename = "plot1.png", width = 480, height = 480 )
-    hist(as.numeric(data[,3]), main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
-    
-    # Close the PNG file device
-    dev.off()
-}
+dataFile <- "./data/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
