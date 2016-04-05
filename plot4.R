b567a3e82b37246505497291d0491bf9eee59ff9
uzera <aashktorab@iso.com>
2016-04-04 22:33:19 -0400

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,50 +1,29 @@
-plot4 <- function() {
-    
-    # Read Data
-    filename <- "household_power_consumption.txt"
-    DF <- read.table(filename, header=TRUE, sep=";", colClasses = "character")
-    
-    # Set required date range
-    beforeDate <- as.Date("2007/02/01", "%Y/%m/%d")
-    afterDate <- as.Date("2007/02/02", "%Y/%m/%d")
-    
-    # retrieve required subset
-    data <- subset(DF, as.Date(DF[,1], "%d/%m/%Y") >= beforeDate & as.Date(DF[,1], "%d/%m/%Y") <= afterDate)
-    
-    # concatenate date and time values and convert to date type
-    tempDateTime <- paste(data[,1], data[,2], sep = ",")
-    tempDateTime <- strptime(tempDateTime, format="%d/%m/%Y,%H:%M:%S")
-    
-    
-    # Create line chart and save as png
-    png(filename = "plot4.png", width = 480, height = 480 )
-    
-    # setting to draw 4 graphs in 2 by 2 format and ordered by plots 1 and 2
-    # followed by plots 3 and 4
-    par(mfrow = c(2,2))
-    
-    # first plot: Date & Time (X) vs Global Active Power (Y)
-    plot(tempDateTime, as.numeric(data[,3]), type = "n", ylab = "Global Active Power", xlab = "")
-    lines(tempDateTime, as.numeric(data[,3]), type = "l")
-    
-    # second plot: Date & Time (X) vs Voltage (Y)
-    plot(tempDateTime, as.numeric(data[,5]), type = "n", ylab = names(data[5]), xlab = "datetime")
-    lines(tempDateTime, as.numeric(data[,5]), type = "l")
-    
-    # third plot: Date & Time (X) vs Sub metering 1, 2 and 3 (Y)
-    plot(tempDateTime, as.numeric(data[,7]), type = "n", ylab = "Energy sub metering", xlab = "")
-    lines(tempDateTime, as.numeric(data[,7]), type = "l")
-    lines(tempDateTime, as.numeric(data[,8]), type = "l", col = "red")
-    lines(tempDateTime, as.numeric(data[,9]), type = "l", col = "blue")
-    legend("topright", col = c("black", "red", "blue"), legend = c(names(data[7]),names(data[8]),names(data[9])), lty = c(1,1), bty = "n")
-    
-    # last plot: Date & Time (X) vs Global Reactive Power (Y)
-    plot(tempDateTime, as.numeric(data[,4]), type = "n", ylab = names(data[4]), xlab = "datetime")
-    lines(tempDateTime, as.numeric(data[,4]), type = "l")
-    
-    
-    # Close the PNG file device
-    dev.off()
-}
-Status API Training Shop Blog About
-© 2016 GitHub, Inc. Terms Privacy Security C
+dataFile <- "./data/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+globalReactivePower <- as.numeric(subSetData$Global_reactive_power)
+voltage <- as.numeric(subSetData$Voltage)
+subMetering1 <- as.numeric(subSetData$Sub_metering_1)
+subMetering2 <- as.numeric(subSetData$Sub_metering_2)
+subMetering3 <- as.numeric(subSetData$Sub_metering_3)
+
+
+png("plot4.png", width=480, height=480)
+par(mfrow = c(2, 2)) 
+
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+
+plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
+
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+
+plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
+
+dev.off()
